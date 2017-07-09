c4f876bfb97c2b8bd36ff632b53b24b19a39a5c2
Dmitri G. Brengauz <dmitri@momus.net>
2017-07-09 17:46:29 -0600

    Man pages??

--- R/fars.R
+++ R/fars.R
@@ -1,16 +1,12 @@
 library(dplyr)
 
-#' Read CSV files into a tibble.
-#'
-#' @description Using \code{\link[dplyr]{tbl_df}} read a file supported by that
-#' function.
-#' 
-#' @param filename String contianing the name and relative path to a
-#'     file.
-#' @return A "tbl_df" tibble object.
-#'
-#' @import dplyr
-#' @export
+##' Read CSV files into a tibble.  Using \code{\link[dplyr]{tbl_df}}
+##' read a file supported by that function.
+##' @title FARS Read Funcion
+##' @param filename String contianing the name and relative path to a
+##'     file.
+##' @return A "tbl_df" tibble object.
+##' @export
 fars_read <- function(filename) {
         if(!file.exists(filename))
                 stop("file '", filename, "' does not exist")
