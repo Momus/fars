b5113b91fc5cda9605ea0bb56edacb87d08e0730
Dmitri G. Brengauz <dmitri@momus.net>
2017-07-09 22:51:13 -0600

    some tests

--- R/fars.R
+++ R/fars.R
@@ -7,6 +7,8 @@ library(dplyr)
 ##'     file.
 ##' @return A "tbl_df" tibble object.
 ##' @export
+##' @examples
+##' \code{\dontrun{data2015 <- fars_read("./data/accident_2015.csv.bz2")}}
 fars_read <- function(filename) {
         if(!file.exists(filename))
                 stop("file '", filename, "' does not exist")
@@ -15,3 +17,35 @@ fars_read <- function(filename) {
         })
         dplyr::tbl_df(data)
 }
+
+#' From the year, create a valid file name.
+#'
+#' @description Using \code{\link[dplyr]{tbl_df}} read a file supported by that
+#' function.
+#' 
+#' @param year A string or integer representation of a year.
+#'     
+#' @return A string matching a valid file name for the dataset.
+#' 
+#' Errors: Anything that can't be coerced by as.integer will
+#' generate a warning, and the same file:
+#' \code{\dontrun{
+#        'zz <- make_filename("zz")
+#'        Warning message:
+#'        In make_filename("zz") : NAs introduced by coercion
+#'       > zz
+#'       [1] "accident_NA.csv.bz2"
+#' }}
+#'
+#' @export
+#' @examples
+#' \code{make_filename(2015)}
+#' 
+make_filename <- function(year) {
+        year <- as.integer(year)
+        sprintf("./data/accident_%d.csv.bz2", year)
+
+}
+
+
+
--- tests/testthat/test_fars_filehandling.R
+++ tests/testthat/test_fars_filehandling.R
@@ -3,5 +3,27 @@ library(fars)
 context("Functions for reading datafiles")
 
 
-expect_error(fars_read("foo"), "file \'foo\' does not exist")
+expect_error(
+    fars_read("foo"),
+    "file \'foo\' does not exist")
+
+## There should be some datafiles for everything to work
+expect_true(
+    file.exists(file.path(system.file(
+        "data", package="fars"), "accident_2014.csv.bz2")))
+
+expect_true(
+    file.exists(file.path(system.file(
+        "data", package="fars"), "accident_2015.csv.bz2")))
+                                  
+## Datafiles read in correctly
+fars_2015 <- fars_read(file.path(system.file(
+    "data", package="fars"), "accident_2015.csv.bz2"))
+
+
+expect_equal(
+    length(fars_2015),
+    52)
+
+
 ##expect_that(make_filename(2014), equals("/home/kwaku/repos/fars_0.0.0.9000.tar.gz"))
