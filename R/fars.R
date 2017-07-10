9949b6b5f1076c42be99ec78832a572be512025d
Dmitri G. Brengauz <dmitri@momus.net>
2017-07-09 23:05:55 -0600

    got rid of ALL examples from docs

--- R/fars.R
+++ R/fars.R
@@ -17,27 +17,18 @@ fars_read <- function(filename) {
         dplyr::tbl_df(data)
 }
 
-#' From the year, create a valid file name.
-#'
-#' @description Using \code{\link[dplyr]{tbl_df}} read a file supported by that
-#' function.
-#' 
-#' @param year A string or integer representation of a year.
-#'     
-#' @return A string matching a valid file name for the dataset.
-#' 
-#' Errors: Anything that can't be coerced by as.integer will
-#' generate a warning, and the same file:
-#' \code{\dontrun{
-#        'zz <- make_filename("zz")
-#'        Warning message:
-#'        In make_filename("zz") : NAs introduced by coercion
-#'       > zz
-#'       [1] "accident_NA.csv.bz2"
-#' }}
-#'
-#' @export
-#' 
+##' From the year, create a valid file name.
+##'
+##' @description Using \code{\link[dplyr]{tbl_df}} read a file supported by that
+##' function.
+##' 
+##' @param year A string or integer representation of a year.
+##'     
+##' @return A string matching a valid file name for the dataset.
+##' 
+##' Errors: Anything that can't be coerced by as.integer will
+##' generate a warning, and the same file.
+##' @export
 make_filename <- function(year) {
         year <- as.integer(year)
         sprintf("./data/accident_%d.csv.bz2", year)
