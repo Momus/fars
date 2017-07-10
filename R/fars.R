2adb9e7aa3c5ee2b48bc30882db57d7540ba2ad8
Dmitri G. Brengauz <dmitri@momus.net>
2017-07-10 17:18:01 -0600

    Got rid of examples. Should work, but does not.

--- R/fars.R
+++ R/fars.R
@@ -79,9 +79,6 @@ fars_read_years <- function(years) {
 ##'     function.
 ##' @import dplyr
 ##' @import tidyr
-##' @examples
-##' fars_summarize_years(c("2014", 2015))
-##' fars_summarize_years(2015)
 ##' @export
 fars_summarize_years <- function(years) {
     dat_list <- fars_read_years(years)
